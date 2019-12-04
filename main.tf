bee435b3dbb6c70a2190b2e9ddb20c7cc913a91c
jdizz <jdizz@users.noreply.github.com>
2019-12-04 13:13:19 -0800

    Breaking files out

--- main.tf
+++ main.tf
@@ -1,24 +1,25 @@
 # Terraform Azure Lab
 # Created: December 03, 2019
 
-# Configure Terraform Azure Provider v1.36.0
-provider "azurerm" {
-    version = "=1.36.0"
-}
-
 # Build a resource group in Oregon
 resource "azurerm_resource_group" "lab" {
-    name        = "lab"
-    location    = "West US 2"
+    name                    = "lab"
+    location                = "West US 2"
 }
 
 # Build a /24 lab network to play around with
 resource "azurerm_virtual_network" "lab" {
-    name                = "lab-network"
-    resource_group_name = azurerm_resource_group.lab.name
-    location            = azurerm_resource_group.location
-    address_space       = ["10.0.0.0/24"]
+    name                    = "lab-network"
+    resource_group_name     = azurerm_resource_group.lab.name
+    location                = azurerm_resource_group.location
+    address_space           = ["10.0.0.0/16"]
 }
 
+resource "azurerm_subnet" "lab_public" {
+    name                    = "lab_public"
+    resource_group_name     = azurerm_resource_group.lab.name
+    virtual_network_name    = azurerm_virtual_network.lab.name
+    address_prefix          = "10.0.2.0/24"
+}
 
 
---
+++ provider.tf
@@ -0,0 +1,4 @@
+# Configure Terraform Azure Provider v1.36.0
+provider "azurerm" {
+    version                 = "=1.36.0"
+}
