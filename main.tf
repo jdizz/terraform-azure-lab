e02683efb6490f1b586822e32e7f2182946e58c0
jdizz <jdizz@users.noreply.github.com>
2019-12-05 10:04:52 -0800

    Restructuring lab network, adding private subnet, and ddos protection

--- main.tf
+++ main.tf
@@ -7,19 +7,45 @@ resource "azurerm_resource_group" "lab" {
     location                = "West US 2"
 }
 
+resource "azurerm_network_security_group" "lab" {
+    name                    = "labSecurityGroupPublic"
+    location                = azurerm_resource_group.lab.location
+    resource_group_name     = azurerm_resource_group.lab.name
+}
+
+resource "azurerm_network_ddos_protection_plan" "lab" {
+    name                    = "labDDOSPlan01"
+    location                = azurerm_resource_group.lab.location
+    resource_group_name     = azurerm_resource_group.lab.name
+}
+
 # Build a /24 lab network to play around with
 resource "azurerm_virtual_network" "lab" {
     name                    = "lab-network"
     resource_group_name     = azurerm_resource_group.lab.name
     location                = azurerm_resource_group.lab.location
     address_space           = ["10.0.0.0/16"]
-}
 
-resource "azurerm_subnet" "lab_public" {
-    name                    = "lab_public"
-    resource_group_name     = azurerm_resource_group.lab.name
-    virtual_network_name    = azurerm_virtual_network.lab.name
-    address_prefix          = "10.0.2.0/24"
+    ddos_protection_plan {
+        id                      = azurerm_network_ddos_protection_plan.lab.id
+        enable                  = true
+    }
+
+    subnet {
+        name                    = "lab_public"
+        address_prefix          = "10.0.2.0/24"
+        security_group          = azurerm_network_security_group.lab.id
+    }
+
+    subnet {
+        name                    = "lab_private"
+        address_prefix          = "10.0.1.0/24"
+    }
+
+    tags = {
+        environment = "lab"
+    }
+
 }
 
 
