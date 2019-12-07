9b0ee9056ed593d2b0e12777ffb0e3025f79fc55
jdizz <jdizz@users.noreply.github.com>
2019-12-06 22:21:30 -0800

    Deploying Kubernetes

--- main.tf
+++ main.tf
@@ -35,6 +35,13 @@ resource "azurerm_virtual_network" "lab" {
         name                    = "lab_public"
         address_prefix          = "10.0.2.0/24"
         security_group          = azurerm_network_security_group.lab.id
+        # delegation {
+        #     name                = "aciDelegation"
+        #     service_delgation {
+        #         name            = "Microsoft.ContainerInstance/containerGroups"
+        #         ations          = ["Microsoft.Network/virtualNetworks/subnets/action"]
+        #     }
+        # }
     }
 
     subnet {
@@ -49,23 +56,48 @@ resource "azurerm_virtual_network" "lab" {
 }
 
 # Deploy Azure Public IP for Load Balancer
-
 resource "azurerm_public_ip" "lab" {
-        name                    = "publicIPforLB"
-        location                = azurerm_resource_group.lab.location
-        resource_group_name     = azurerm_resource_group.lab.name
-        allocation_method       = "Static"
+    name                    = "publicIPforLB"
+    location                = azurerm_resource_group.lab.location
+    resource_group_name     = azurerm_resource_group.lab.name
+    allocation_method       = "Static"
 }
 
 # Deploy Azure Load Balancer
-
 resource "azurerm_lb" "lab" {
-        name                    = "LabLoadBalancer"
-        location                = azurerm_resource_group.lab.location
-        resource_group_name     = azurerm_resource_group.lab.name
+    name                    = "LabLoadBalancer"
+    location                = azurerm_resource_group.lab.location
+    resource_group_name     = azurerm_resource_group.lab.name
 
-        frontend_ip_configuration {
-            name                = "PublicIPAddress"
-            public_ip_address_id = azurerm_public_ip.lab.id
+    frontend_ip_configuration {
+        name                = "PublicIPAddress"
+        public_ip_address_id = azurerm_public_ip.lab.id
         }
+}
+
+# Deploy Azure Kubernetes Cluster
+resource "azurerm_kubernetes_cluster" "lab" {
+    name                    = "lab-aks1"
+    location                = azurerm_resource_group.lab.location
+    resource_group_name     = azurerm_resource_group.lab.name
+    dns_prefix              = "labaks1"
+
+    default_node_pool {
+        name                = "default"
+        enable_auto_scaling = true
+        max_count           = 1
+        min_count           = 1
+        node_count          = 1
+        type                = VirtualMachineScaleSets
+        vm_size             = "Standard_B1ls"
+    }
+
+    service_principal {
+        client_id           = "00000000-0000-0000-0000-000000000000"
+        client_secret       = "00000000000000000000000000000000"
+    }
+
+    tags = {
+        Environment         = "lab"
+    }
 }
